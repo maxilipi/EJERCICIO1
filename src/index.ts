d9b02c4eb0f0c6da45c91a5bab232b25406430e3
maxilipi <104212047+maxilipi@users.noreply.github.com>
2022-04-25 20:39:48 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let precio: number = 450.5;
+let descuento: number = precio * 0.1;
+let preciototal: number = precio - descuento;
+
+console.log("el precio es", precio);
+console.log("el descuento es", descuento);
+console.log("el precio total es", preciototal);
